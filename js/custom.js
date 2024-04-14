0440f8d24e8a9c3159b7f5da36f24ef82fcc3622
Adiy03 <adiyogo3@gmail.com>
2024-04-15 06:28:38 +0700

    asd

---
+++ js/custom.js
@@ -0,0 +1,118 @@
+// navigation  menu js
+function openNav() {
+    $("#myNav").addClass("menu_width");
+    $(".menu_btn-style").fadeIn();
+}
+
+function closeNav() {
+    $("#myNav").removeClass("menu_width");
+    $(".menu_btn-style").fadeOut();
+}
+
+// get current year
+
+function displayYear() {
+    var d = new Date();
+    var currentYear = d.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+displayYear();
+
+
+
+//client section owl carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+// slider carousel control
+
+
+$('.slider_btn_prev').on('click', function (e) {
+    e.preventDefault()
+    $('.slider_text_carousel').carousel('prev')
+    $('.slider_image_carousel').carousel('prev')
+})
+
+
+$('.slider_btn_next').on('click', function (e) {
+    e.preventDefault()
+    $('.slider_text_carousel').carousel('next')
+    $('.slider_image_carousel').carousel('next')
+})
+
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+
+// Panels
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>{
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active")
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
+
+// Get the modal element
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var modalButton = document.getElementById("modalButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+modalButton.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
